abd3c2cf0fab7dc2012a9a71afb6461463b4d112
Naveen M V <naveenmv7@gmail.com>
2018-06-01 18:18:19 +0530

    Implement skeleton UI

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 from flask import jsonify
 from sqlalchemy import and_, or_
@@ -7,84 +7,185 @@ from app import app
 from app.models import *
 
 
-@app.route('/')
-def index():
+
+
+@app.route('/', methods=['POST', 'GET'], defaults={'exam_id': None, 'college_id': None, 'department_id': None})
+@app.route("/exams/<int:exam_id>", defaults={'college_id': None, 'department_id': None})
+@app.route("/exams/<int:exam_id>/colleges/<int:college_id>", defaults={'department_id': None})
+@app.route("/exams/<int:exam_id>/colleges/<int:college_id>/departments/<int:department_id>")
+def index(exam_id, college_id, department_id):
+
     exam_list = []
+
     for exam in Exam.query.all():
         new_exam = {}
         new_exam['id'] = exam.id
         new_exam['name'] = exam.name
         exam_list.append(new_exam)
 
-    return render_template('index.html', exam_list=exam_list)
 
-@app.route("/api/exams/<int:exam_id>", defaults={'college_id': None, 'department_id': None})
-@app.route("/api/exams/<int:exam_id>/colleges/<int:college_id>", defaults={'department_id': None})
-@app.route("/api/exams/<int:exam_id>/colleges/<int:college_id>/departments/<int:department_id>")
-def exam_api(exam_id, college_id, department_id):
-    """Return details about exam."""
 
-    # Make sure exam exists.
-    
-    exam = Exam.query.get(exam_id)
-    if exam is None:
-        return jsonify({"error": "Invalid exam_id"}), 422
+    if request.method == 'POST':
+        exam_id = request.form.get('exam')
+        exam = Exam.query.get(exam_id)
 
-    if college_id is None:
-        # Get all result.
-        cpercent_list = []
+        exam_details = {'exam_name': exam.name, 'exam_id': exam.id}
+
+        if exam is None:
+            return "<h1> Error : Invalid exam id </h1>", 422
+
+        pass_list = []
         college_ranks = exam.college_ranks
-        
+
         for clg in college_ranks:
-            cpercent_list.append([College.query.get(clg.college_id).name, clg.pass_percent])
+            pass_list.append({ 
+                               'name': College.query.get(clg.college_id).name,
+                               'pass_percent': clg.pass_percent,
+                               'id': clg.college_id
+                              })
+        return render_template('exam.html', pass_list=pass_list, exam_details=exam_details, exam_list=exam_list)
+                              
 
-        return jsonify({
-                "exam_name": exam.name,
-                "college_pass_percentage": cpercent_list
-            })
-    else:
+    elif college_id and not department_id:
+
+        exam = Exam.query.get(exam_id)
         college = College.query.get(college_id)
-        if college is None:
-                return jsonify({"error": "Invalid college_id"}), 422
-        if department_id is None:
-            dpercent_list = []
+        exam_details = {'exam_name': exam.name, 'exam_id': exam.id, 'college_name': college.name, 'college_id': college.id}
 
-            cpp = CollegeRank.query.filter(and_(CollegeRank.college_id==college_id, CollegeRank.exam_id==exam_id)).first().pass_percent
-            department_ranks = DepartmentRank.query.filter(and_(DepartmentRank.exam_id==exam_id, DepartmentRank.college_id==college_id)).all()
-            
-            for dept in department_ranks:
-                dpercent_list.append([Department.query.get(dept.department_id).name, dept.pass_percent])
+        department_ranks = DepartmentRank.query.filter(and_(DepartmentRank.exam_id==exam_id, DepartmentRank.college_id==college_id)).all()
+        pass_list = []
+        for dept in department_ranks:
+            pass_list.append({ 
+                                'name': Department.query.get(dept.department_id).name,
+                                'pass_percent': dept.pass_percent,
+                                'id': dept.department_id
+                                })
+        return render_template('college.html', pass_list=pass_list, exam_details=exam_details, exam_list=exam_list)
+                                
+    elif department_id:
+        exam = Exam.query.get(exam_id)
+        college = College.query.get(college_id)
+        department = Department.query.get(department_id);
+        exam_details = {'exam_name': exam.name, 'exam_id': exam.id, 'college_name': college.name, 'college_id': college.id,
+                        'department_name': department.name, 'department_id': department.id}
 
-            return jsonify({
-                    "exam_name": exam.name,
-                    "college_name": college.name,
-                    "college_pass_percent": cpp,
-                    "department_pass_percentage": dpercent_list
-                })
-        else:
-            department = Department.query.get(department_id)
-            if department is None:
-                return jsonify({"error": "Invalid department_id"}), 422
-            
-            dpp = DepartmentRank.query.filter(and_(DepartmentRank.exam_id==exam_id,
-                                                    DepartmentRank.college_id==college_id,
-                                                    DepartmentRank.department_id==department_id)).first().pass_percent
-
-            sgpa = []
-            sl = Student.query.filter(and_(Student.exam_id==exam_id,
+        student_ranks = Student.query.filter(and_(Student.exam_id==exam_id,
                                     Student.college_id==college_id,
                                     Student.department_id==department_id)).all()
-            for s in sl:
-                sgpa.append([s.register_no, s.sgpa])
+        sgpa_list = []
+        for student in student_ranks:
+            sgpa_list.append({
+                'register_no': student.register_no,
+                'sgpa': student.sgpa
+            })
+        return render_template('department.html', sgpa_list=sgpa_list, exam_details=exam_details, exam_list=exam_list)
+        
+
+    else:
+        return render_template('index.html', exam_list=exam_list)
+        
+
+
+
+# API Sample Implementation
+
+        
+
+# @app.route("/exams/<int:exam_id>/colleges/<int:college_id>")
+# def department(exam_id, college_id):
+#     exam_list = []
+
+#     for exam in Exam.query.all():
+#         new_exam = {}
+#         new_exam['id'] = exam.id
+#         new_exam['name'] = exam.name
+#         exam_list.append(new_exam)
+
+
+#     exam = Exam.query.get(exam_id)
+#     college = College.query.get(college_id)
+#     exam_details = {'exam_name': exam.name, 'exam_id': exam.id, 'college_name': college.name, 'college_id': college.id}
+
+#     department_ranks = DepartmentRank.query.filter(and_(DepartmentRank.exam_id==exam_id, DepartmentRank.college_id==college_id)).all()
+#     pass_list = []
+#     for dept in department_ranks:
+#         pass_list.append({ 
+#                                'name': Department.query.get(dept.department_id).name,
+#                                'pass_percent': dept.pass_percent,
+#                                'id': dept.department_id
+#                               })
+
+#     return render_template('rank.html', pass_list=pass_list, exam_details=exam_details, exam_list=exam_list)
+
+
+
+# @app.route("/api/exams/<int:exam_id>", defaults={'college_id': None, 'department_id': None})
+# @app.route("/api/exams/<int:exam_id>/colleges/<int:college_id>", defaults={'department_id': None})
+# @app.route("/api/exams/<int:exam_id>/colleges/<int:college_id>/departments/<int:department_id>")
+# def exam_api(exam_id, college_id, department_id):
+#     """Return details about exam."""
+
+#     # Make sure exam exists.
+    
+#     exam = Exam.query.get(exam_id)
+#     if exam is None:
+#         return jsonify({"error": "Invalid exam_id"}), 422
+
+#     if college_id is None:
+#         # Get all result.
+#         cpercent_list = []
+#         college_ranks = exam.college_ranks
+        
+#         for clg in college_ranks:
+#             cpercent_list.append([College.query.get(clg.college_id).name, clg.pass_percent])
+
+#         return jsonify({
+#                 "exam_name": exam.name,
+#                 "college_pass_percentage": cpercent_list
+#             })
+#     else:
+#         college = College.query.get(college_id)
+#         if college is None:
+#                 return jsonify({"error": "Invalid college_id"}), 422
+#         if department_id is None:
+#             dpercent_list = []
+
+#             cpp = CollegeRank.query.filter(and_(CollegeRank.college_id==college_id, CollegeRank.exam_id==exam_id)).first().pass_percent
+#             department_ranks = DepartmentRank.query.filter(and_(DepartmentRank.exam_id==exam_id, DepartmentRank.college_id==college_id)).all()
+            
+#             for dept in department_ranks:
+#                 dpercent_list.append([Department.query.get(dept.department_id).name, dept.pass_percent])
+
+#             return jsonify({
+#                     "exam_name": exam.name,
+#                     "college_name": college.name,
+#                     "college_pass_percent": cpp,
+#                     "department_pass_percentage": dpercent_list
+#                 })
+#         else:
+#             department = Department.query.get(department_id)
+#             if department is None:
+#                 return jsonify({"error": "Invalid department_id"}), 422
+            
+#             dpp = DepartmentRank.query.filter(and_(DepartmentRank.exam_id==exam_id,
+#                                                     DepartmentRank.college_id==college_id,
+#                                                     DepartmentRank.department_id==department_id)).first().pass_percent
+
+#             sgpa = []
+#             sl = Student.query.filter(and_(Student.exam_id==exam_id,
+#                                     Student.college_id==college_id,
+#                                     Student.department_id==department_id)).all()
+#             for s in sl:
+#                 sgpa.append([s.register_no, s.sgpa])
 
             
 
-            return jsonify({
-                "exam_name": exam.name,
-                "college_name": college.name,
-                "department_name": department.name,
-                "department_pass_percent": dpp,
-                "sgpa": sgpa
-            })
+#             return jsonify({
+#                 "exam_name": exam.name,
+#                 "college_name": college.name,
+#                 "department_name": department.name,
+#                 "department_pass_percent": dpp,
+#                 "sgpa": sgpa
+#             })
 
             
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,77 +1,77 @@
-// window.alert("Helo")
-document.getElementById("exam-btn").addEventListener("click", function(){
-    var exam_id = document.getElementById('my-select').value
-    load_page(exam_id);
-});
+// // window.alert("Helo")
+// document.getElementById("exam-btn").addEventListener("click", function(){
+//     var exam_id = document.getElementById('my-select').value
+//     load_page(exam_id);
+// });
 
 
-function load_page(exam_id, college_id = null, dept_id = null) {
-    const request = new XMLHttpRequest();
-    if(! (college_id || dept_id))
-        request.open('GET', `/api/exams/${exam_id}`);
-    else if(college_id)
-        request.open('GET', `/api/exam/${exam_id}/colleges/${college_id}`);
-    else if(dept_id)
-        request.open('GET', `/api/exam/${exam_id}/colleges/${college_id}/departments/${dept_id}`);
+// function load_page(exam_id, college_id = null, dept_id = null) {
+//     const request = new XMLHttpRequest();
+//     if(! (college_id || dept_id))
+//         request.open('GET', `/api/exams/${exam_id}`);
+//     else if(college_id)
+//         request.open('GET', `/api/exam/${exam_id}/colleges/${college_id}`);
+//     else if(dept_id)
+//         request.open('GET', `/api/exam/${exam_id}/colleges/${college_id}/departments/${dept_id}`);
     
-    request.onload = () => {
-        const data = JSON.parse(request.responseText);
-        createTable(data);
-    }
-    request.send()
+//     request.onload = () => {
+//         const data = JSON.parse(request.responseText);
+//         createTable(data);
+//     }
+//     request.send()
 
         
-}
+// }
 
-function createTable(data) {
-    data['college_pass_percentage'].sort(function (a,b) {
-        return a[1] > b[1] ? -1 : 1;
-    });
-    var body = document.getElementsByTagName('body')[0];
-    var tbl = document.createElement('table');
-    // tbl.createCaption();
-    // tbl.innerHTML = `<b> ${data['exam_name']} </b>`;
-    tbl.style.width = '100%';
-    tbl.setAttribute('border', '1');
-    var tbdy = document.createElement('tbody');
+// function createTable(data) {
+//     data['college_pass_percentage'].sort(function (a,b) {
+//         return a[1] > b[1] ? -1 : 1;
+//     });
+//     var body = document.getElementsByTagName('body')[0];
+//     var tbl = document.createElement('table');
+//     // tbl.createCaption();
+//     // tbl.innerHTML = `<b> ${data['exam_name']} </b>`;
+//     tbl.style.width = '100%';
+//     tbl.setAttribute('border', '1');
+//     var tbdy = document.createElement('tbody');
 
-    var tr = document.createElement('tr');
-    var th = document.createElement('th');
-    th.appendChild(document.createTextNode('Rank'));
-    tr.appendChild(th)
+//     var tr = document.createElement('tr');
+//     var th = document.createElement('th');
+//     th.appendChild(document.createTextNode('Rank'));
+//     tr.appendChild(th)
 
-    th = document.createElement('th');
-    th.appendChild(document.createTextNode('College Name'));
-    tr.appendChild(th)
+//     th = document.createElement('th');
+//     th.appendChild(document.createTextNode('College Name'));
+//     tr.appendChild(th)
 
-    th = document.createElement('th');
-    th.appendChild(document.createTextNode('Pass Percentage'));
-    tr.appendChild(th)
-    tbdy.appendChild(tr);
+//     th = document.createElement('th');
+//     th.appendChild(document.createTextNode('Pass Percentage'));
+//     tr.appendChild(th)
+//     tbdy.appendChild(tr);
     
-    for(var i = 0; i < data['college_pass_percentage'].length; i++)
-    {
-        var tr = document.createElement('tr');
-        for(var j = 0; j < 3; j++)
-        {
-            var td = document.createElement('td');
-            if(j == 0) {
-                td.appendChild(document.createTextNode(`${i+1}`));
-                td.setAttribute('align', 'center');
-            }
-            else if(j == 1) {
-                td.appendChild(document.createTextNode(`${data['college_pass_percentage'][i][j-1]}`));
-            }
-            else {
-                td.appendChild(document.createTextNode(`${data['college_pass_percentage'][i][j-1].toFixed(2)}`));
-                td.setAttribute('align', 'center');
-            }
+//     for(var i = 0; i < data['college_pass_percentage'].length; i++)
+//     {
+//         var tr = document.createElement('tr');
+//         for(var j = 0; j < 3; j++)
+//         {
+//             var td = document.createElement('td');
+//             if(j == 0) {
+//                 td.appendChild(document.createTextNode(`${i+1}`));
+//                 td.setAttribute('align', 'center');
+//             }
+//             else if(j == 1) {
+//                 td.appendChild(document.createTextNode(`${data['college_pass_percentage'][i][j-1]}`));
+//             }
+//             else {
+//                 td.appendChild(document.createTextNode(`${data['college_pass_percentage'][i][j-1].toFixed(2)}`));
+//                 td.setAttribute('align', 'center');
+//             }
                 
-            tr.appendChild(td)
-        }
-        tbdy.appendChild(tr);
-    }
-    tbl.appendChild(tbdy);
-    body.appendChild(tbl);
-}
+//             tr.appendChild(td)
+//         }
+//         tbdy.appendChild(tr);
+//     }
+//     tbl.appendChild(tbdy);
+//     body.appendChild(tbl);
+// }
 
